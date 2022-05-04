c1a59d5b0742f980d0434679599837401ca8926a
Juliana Weinert <julianalima@aluno.santoangelo.uri.br>
2022-05-04 11:28:08 -0300

    subindo o projeto spring boot

---
+++ src/main/java/com/julianawl/dio/bankline/api/BanklineApiApplication.java
@@ -0,0 +1,13 @@
+package com.julianawl.dio.bankline.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julianawl/dio/bankline/api/BanklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianawl.dio.bankline.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
