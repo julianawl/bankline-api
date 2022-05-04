8ee91fc7edf3514e97e558437daa097a9c0cf8f8
Juliana Weinert <julianalima@aluno.santoangelo.uri.br>
2022-05-04 13:56:54 -0300

    mapeando entidades com jpa

--- src/main/java/com/julianawl/dio/bankline/api/model/Conta.java
+++ src/main/java/com/julianawl/dio/bankline/api/model/Conta.java
@@ -1,7 +1,13 @@
 package com.julianawl.dio.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
+	@Column(name = "conta_numero")
 	private Long numero;
+	@Column(name = "conta_saldo")
     private Double saldo;
 
     public Long getNumero() {
--- src/main/java/com/julianawl/dio/bankline/api/model/Correntista.java
+++ src/main/java/com/julianawl/dio/bankline/api/model/Correntista.java
@@ -1,10 +1,18 @@
 package com.julianawl.dio.bankline.api.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
-	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column(length = 20)
     private String cpf;
+	@Column(length = 60)
     private String nome;
+	@Embedded
     private Conta conta;
 
     public Conta getConta() {
